1dd5a8bfd552141c8ca4e3bec914d160bc754a09
MiracleM4n <MiracleM4n@gmail.com>
2014-09-08 03:01:11 -0400

    [Bug Fix] Change Chat events back to Async.

--- src/main/java/ca/q0r/madvanced/events/ChatListener.java
+++ src/main/java/ca/q0r/madvanced/events/ChatListener.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.util.Date;
 import java.util.UUID;
@@ -18,7 +18,7 @@ public class ChatListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerChat(PlayerChatEvent event) {
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
         if (event.isCancelled()) {
             return;
         }
